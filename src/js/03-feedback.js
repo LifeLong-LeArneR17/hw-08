ad75dfaa7eafe324c304bfca5bac634adc55062e
LifeLong-LeArneR17 <sdelay.ne.sam@gmail.com>
2023-03-04 21:57:30 +0200

    fix
    fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+const form = document.querySelector('.feedback-form');
+const userInfo = {};
+
+const onContactFormFieldChange = event => {
+    event.preventDefault()
+    const { target } = event
+    const fieldValue = target.value;
+    const fieldName = target.name;
+    userInfo[fieldName] = fieldValue;
+    localStorage.setItem("feedback-form-state", JSON.stringify(userInfo))
+   
+}
+
+const onContactFormSubmit = event => {
+    event.preventDefault();
+    form.reset();
+    const savedData = localStorage.getItem("feedback-form-state");
+    const userData = JSON.parse(savedData);
+    console.log(userData);
+    if (userData === undefined) {
+        return;
+    }
+    localStorage.removeItem('feedback-form-state');
+}
+
+
+form.addEventListener('input', onContactFormFieldChange)
+form.addEventListener('submit', onContactFormSubmit)
+
+
+
+
+
+
 

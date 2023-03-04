14de2a87c83975cd8ec17f29bd58bbe2010e7df8
LifeLong-LeArneR17 <sdelay.ne.sam@gmail.com>
2023-03-04 16:24:48 +0200

    fix
    fix

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+
+// Change code below this line
+const galleryImages = document.querySelector('.gallery');
+const galleryEm = galleryItems => {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}
+
+"/>
+ </a>
+        </div>`;
+    })
+        .join("")
+}
+
+const addGalleryMarkup = galleryEm(galleryItems);
+galleryImages.innerHTML = addGalleryMarkup;
+
+
+
+let gallery = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionsData: 'alt',
+});
+
+
+
